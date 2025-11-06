b07fe8938ede79bd6f5b16ba7d0cb1d213134b25
Rannssuuu <peruslance@gmail.com>
2025-11-06 23:55:55 +0800

    feat: added auth routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,1 +1,32 @@
-// placeholder
+// routes/authRoutes.js
+
+import express from "express";
+
+const router = express.Router();
+
+/**
+ *  GET /auth/login
+ *  Public
+ */
+router.get("/login", oauthLogin);
+
+/**
+ * GET /auth/callback
+ * Public
+ */
+router.get("/callback", oauthCallback);
+
+/**
+ * POST /auth/logout
+ * Authenticated users
+ */
+router.post("/logout", logoutUser);
+
+/**
+ * GET /auth/me
+ * Frontend calls this to check if user is logged in and to get their info
+ * Authenticated users
+ */
+router.get("/me", getCurrentUser);
+
+export default router;
