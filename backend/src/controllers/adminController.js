e31dfce129ccb5a459d1b032a961602135ba0f3a
Rannssuuu <peruslance@gmail.com>
2026-01-13 17:08:58 +0800

    fix: typo change Pending->pending

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -4,7 +4,7 @@ import Report from '../models/Report.js';
 export const getDashboardStats = async (req, res) => {
   try {
     const totalTutors = await User.countDocuments({ role: 'tutor' });
-    const unresolvedReports = await Report.countDocuments({ status: 'Pending' });
+    const unresolvedReports = await Report.countDocuments({ status: 'pending' });
     res.status(200).json({ totalTutors, unresolvedReports });
   } catch (error) {
     res.status(500).json({ message: error.message });
